79c5e8559c50c8c4c762418136325adf410f841d
mehmet.alp <mehmet.alp@ginitalent.com>
2024-09-23 23:47:19 +0300

    Add core request method to handle API requests

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 class ApiClient {
     constructor(baseURL) {
-      this.baseURL = baseURL || '';
-      this.defaultHeaders = {
-        'Content-Type': 'application/json'
-      };
+        this.baseURL = baseURL || '';
+        this.defaultHeaders = {
+            'Content-Type': 'application/json'
+        };
     }
 
     setHeaders(headers) {
         this.defaultHeaders = { ...this.defaultHeaders, ...headers };
-      }
+    }
+
+
+    request(endpoint, options = {}) {
+        let config = {
+            method: options.method || 'GET',
+            headers: { ...this.defaultHeader, ...options.headers }
+        };
+        if (options.body) {
+            config.body = JSON.stringify(options.body);
+        }
+        const response = fetch(this.baseURL + endpoint, config);
+        return response;
+    }
 
 }
 
 module.exports = ApiClient;
 
-  
